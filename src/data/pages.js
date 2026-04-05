5e08f7a0f735218527fc5ba96cda452490849ff6
Clément Tarrière <clement.tarriere@gmail.com>
2026-04-05 13:06:11 +0200

    Fix wrong answer

--- src/data/pages.js
+++ src/data/pages.js
@@ -4,21 +4,65 @@
 
 export const pages = [
   { id: "oeuf-01", photos: ["oeuf-01a.jpg", "oeuf-01b.jpg"], correct: "Jules" },
-  { id: "oeuf-02", photos: ["oeuf-02a.jpg", "oeuf-02b.jpg"], correct: "Aurélie" },
-  { id: "oeuf-03", photos: ["oeuf-03a.jpg", "oeuf-03b.jpg"], correct: "Clément" },
+  {
+    id: "oeuf-02",
+    photos: ["oeuf-02a.jpg", "oeuf-02b.jpg"],
+    correct: "Aurélie",
+  },
+  {
+    id: "oeuf-03",
+    photos: ["oeuf-03a.jpg", "oeuf-03b.jpg"],
+    correct: "Aurélie",
+  },
   { id: "oeuf-04", photos: ["oeuf-04a.jpg", "oeuf-04b.jpg"], correct: "Jules" },
   { id: "oeuf-05", photos: ["oeuf-05a.jpg", "oeuf-05b.jpg"], correct: "Jules" },
-  { id: "oeuf-06", photos: ["oeuf-06a.jpg", "oeuf-06b.jpg"], correct: "Clément" },
-  { id: "oeuf-07", photos: ["oeuf-07a.jpg", "oeuf-07b.jpg"], correct: "Clément" },
+  {
+    id: "oeuf-06",
+    photos: ["oeuf-06a.jpg", "oeuf-06b.jpg"],
+    correct: "Clément",
+  },
+  {
+    id: "oeuf-07",
+    photos: ["oeuf-07a.jpg", "oeuf-07b.jpg"],
+    correct: "Clément",
+  },
   { id: "oeuf-08", photos: ["oeuf-08a.jpg", "oeuf-08b.jpg"], correct: "Jules" },
-  { id: "oeuf-09", photos: ["oeuf-09a.jpg", "oeuf-09b.jpg"], correct: "Aurélie" },
+  {
+    id: "oeuf-09",
+    photos: ["oeuf-09a.jpg", "oeuf-09b.jpg"],
+    correct: "Aurélie",
+  },
   { id: "oeuf-10", photos: ["oeuf-10a.jpg", "oeuf-10b.jpg"], correct: "Jules" },
-  { id: "oeuf-11", photos: ["oeuf-11a.jpg", "oeuf-11b.jpg"], correct: "Clément" },
-  { id: "oeuf-12", photos: ["oeuf-12a.jpg", "oeuf-12b.jpg"], correct: "Aurélie" },
-  { id: "oeuf-13", photos: ["oeuf-13a.jpg", "oeuf-13b.jpg"], correct: "Aurélie" },
-  { id: "oeuf-14", photos: ["oeuf-14a.jpg", "oeuf-14b.jpg"], correct: "Clément" },
-  { id: "oeuf-15", photos: ["oeuf-15a.jpg", "oeuf-15b.jpg"], correct: "Clément" },
+  {
+    id: "oeuf-11",
+    photos: ["oeuf-11a.jpg", "oeuf-11b.jpg"],
+    correct: "Clément",
+  },
+  {
+    id: "oeuf-12",
+    photos: ["oeuf-12a.jpg", "oeuf-12b.jpg"],
+    correct: "Aurélie",
+  },
+  {
+    id: "oeuf-13",
+    photos: ["oeuf-13a.jpg", "oeuf-13b.jpg"],
+    correct: "Aurélie",
+  },
+  {
+    id: "oeuf-14",
+    photos: ["oeuf-14a.jpg", "oeuf-14b.jpg"],
+    correct: "Clément",
+  },
+  {
+    id: "oeuf-15",
+    photos: ["oeuf-15a.jpg", "oeuf-15b.jpg"],
+    correct: "Clément",
+  },
   { id: "oeuf-16", photos: ["oeuf-16a.jpg", "oeuf-16b.jpg"], correct: "Jules" },
   { id: "oeuf-17", photos: ["oeuf-17a.jpg", "oeuf-17b.jpg"], correct: "Jules" },
-  { id: "oeuf-18", photos: ["oeuf-18a.jpg", "oeuf-18b.jpg"], correct: "Aurélie" },
+  {
+    id: "oeuf-18",
+    photos: ["oeuf-18a.jpg", "oeuf-18b.jpg"],
+    correct: "Aurélie",
+  },
 ];
